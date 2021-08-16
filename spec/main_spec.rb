71b4daf084e3814eba583940dafad474852972ed
Agustin Hodes <hodesagustin@gmail.com>
2021-08-16 14:55:34 -0300

    Estructura para proyecto RubyMine.
    Update gitignore.

---
+++ spec/main_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../src/main'
+
+describe "Main Tests" do
+
+  it "sumar 3 + 2 should be 5" do
+    expect(Calculadora.sumar(3, 2)).eql?(5)
+  end
+
+  it "dividir por 0 should throw exception" do
+    expect { Calculadora.dividir(4, 0) }.to raise_exception(ZeroDivisionError)
+  end
+end
---
+++ src/main.rb
@@ -0,0 +1,9 @@
+class Calculadora
+  def self.sumar(a, b)
+    a + b
+  end
+
+  def self.dividir(a, b)
+    a / 0
+  end
+end
